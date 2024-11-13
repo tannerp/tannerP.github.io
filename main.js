1e62c6829e32a756b63afb5687642d1845fdb9ad
tannerp <tannerphan@gmail.com>
2024-11-12 16:22:58 -0800

    VN music playlist

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div class="video-container">
-      <iframe class="video"
-      src="https://www.youtube.com/embed/videoseries?si=CGfLz7pGdBv6sEi9&amp;list=PLZm7JLb1oQvKZ04Hn_hmB2H7Vm_8sIpg5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe class="video" src="https://www.youtube.com/embed/videoseries?si=cJXEBr3DQelkgms-&amp;list=PLZm7JLb1oQvLaFPVSaki9FWkkO03c-yfs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   </div>
 `
 
 
 document.querySelector('head').html = `
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NF8BWJ4213"></script>
+<script async src="https://youtube.com/playlist?list=PLZm7JLb1oQvLaFPVSaki9FWkkO03c-yfs&si=2NCM6b8AvZMCXobZ"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
